a95755a316bd9c4bd8f3f402f3009dc1d0441a8d
no7here <102915075+no7here@users.noreply.github.com>
2026-06-06 11:57:02 +0100

    Add embed helper file + Added all 4 embed types (success, warning, error, info) + Fetches emojis and colour from another utility file (yet to be written, but will be in helpers.py)

---
+++ utilities/embeds.py
@@ -0,0 +1,19 @@
+import discord
+from utilities.helpers import get_emoji, get_colour
+
+class Embeds:
+    @staticmethod
+    def success(description: str) -> discord.Embed:
+        return discord.Embed(description=f"{get_emoji("success")} {description}", color=get_color("success"))
+
+    @staticmethod
+    def warning(description: str) -> discord.Embed:
+        return discord.Embed(description=f"{get_emoji("warning")} {description}", color=get_color("warning"))
+
+    @staticmethod
+    def error(description: str) -> discord.Embed:
+        return discord.Embed(description=f"{get_emoji("error")} {description}", color=get_color("error"))
+
+    @staticmethod
+    def info(description: str) -> discord.Embed:
+        return discord.Embed(description=f"{get_emoji("info")} {description}", color=get_color("info"))
