38db06fda95d670668330279d162629863b15668
no7here <102915075+no7here@users.noreply.github.com>
2026-06-30 15:19:18 +0100

    Bug fixes & permission check + Fixed slash command error handler not handling... errors... + Added global permission check + Fixed exceptions not logging the actual exception, just the class name "exception" + Fixed command error handlers and on_ready functions by moving them inside the setup_hook     + Removed the @self.client part (which didn't work anyway)     + Added self to arguments of functions     + Corrected on_ready username printer to actually print the username instead of error + Fixed some spelling mistakes like command_prefix being spelt with an unnecessary "s"

--- index.py
+++ index.py
@@ -12,10 +12,16 @@ from utilities.output import Logger
 
 class PookieBot(commands.Bot):
     def __init__(self):
-        super().__init__(commands_prefix="", intents=discord.Intents.all(), help_command=None)
+        super().__init__(command_prefix="", intents=discord.Intents.all(), help_command=None)
 
     async def setup_hook(self):
         await init_db()
+
+        # Hook slash command errors directly to command tree
+        self.tree.on_error = self.on_app_command_error
+
+        # Global permission check
+        self.tree.interaction_check = is_verified
 
         # Fetch all modules and their state from the config
         module_config = config.get("modules", {})
@@ -26,71 +32,79 @@ class PookieBot(commands.Bot):
                 try:
                     await self.load_extension(f"modules.{module_name}")
                     Logger.success(f"Loaded {module_name}")
-                except:
-                    Logger.error(f"Failed to load {module_path}. Check log.", Exception)
+                except Exception as e:
+                    Logger.error(f"Failed to load {module_name}. Check log.", str(e))
 
         # Sync bot commands to Discord
         try:
             synced = await self.tree.sync()
             Logger.info(f"Synced {len(synced)} commands")
-        except:
-            Logger.error(f"Failed to sync commands. Check log.", Exception)
+        except Exception as e:
+            Logger.error(f"Failed to sync commands. Check log.", str(e))
+
+    # Legacy command error handler
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            return
+        Logger.error(f"Ignoring exception in command {ctx.command}. Check log.", error)
+
+    # Slash command error handler
+    async def on_app_command_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        # If user is missing permissions
+        if isinstance(error, app_commands.MissingPermissions):
+            embed = Embeds.error(f"You don't have permission to do that.")
+
+            if interaction.response.is_done():
+                await interaction.followup.send(embed=embed, ephemeral=True)
+            else:
+                await interaction.response.send_message(embed=embed, ephemeral=True)
+            return
+
+        # If bot is missing permissions
+        elif isinstance(error, app_commands.BotMissingPermissions):
+            embed = Embeds.error(f"I don't have permission to do that.")
+
+            if interaction.response.is_done():
+                await interaction.followup.send(embed=embed, ephemeral=True)
+            else:
+                await interaction.response.send_message(embed=embed, ephemeral=True)
+            return
+
+        # If user is executing commands too fast
+        elif isinstance(error, app_commands.CommandOnCooldown):
+            embed = Embeds.error(f"You've been rate limited. Try again in {error.retry_after:.1f} seconds.")
+
+            if interaction.response.is_done():
+                await interaction.followup.send(embed=embed, ephemeral=True)
+            else:
+                await interaction.response.send_message(embed=embed, ephemeral=True)
+            return
+
+        # If user fails global permission checks
+        elif isinstance(error, app_commands.CheckFailure):
+            embed = Embeds.error("You must be verified to use commands in this server.")
+
+            if interaction.response.is_done():
+                await interaction.followup.send(embed=embed, ephemeral=True)
+            else:
+                await interaction.response.send_message(embed=embed, ephemeral=True)
+            return
+
+        # Handle generic error messages
+        else:
+            embed = Embeds.error(f"An unexpected error occurred.")
+
+            if interaction.response.is_done():
+                await interaction.followup.send(embed=embed, ephemeral=True)
+            else:
+                await interaction.response.send_message(embed=embed, ephemeral=True)
+            return
+
+    async def on_ready(self):
+        Logger.info(f"Connected to Discord as {self.user.name}#{self.user.discriminator}")
+
 
 client = PookieBot()
-
-# Legacy command error handler
-@client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CheckFailure):
-        return
-    Logger.error(f"Ignoring exception in command {ctx.command}. Check log.", {error})
-
-# Slash command error handler
-@client.event
-async def on_app_command_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
-    # If user is missing permissions
-    if isinstance(error, app_commands.MissingPermissions):
-        embed = Embeds.error(f"You don't have permission to do that.")
-
-        if interaction.response.is_done():
-            await interaction.followup.send(embed=embed, ephemeral=True)
-        else:
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-        return
-
-    # If bot is missing permissions
-    elif isinstance(error, app_commands.BotMissingPermissions):
-        embed = Embeds.error(f"I don't have permission to do that.")
-
-        if interaction.response.is_done():
-            await interaction.followup.send(embed=embed, ephemeral=True)
-        else:
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-        return
-
-    # If user is executing commands too fast
-    elif isinistance(error, app_commands.CommandOnCooldown):
-        embed = Embeds.error(f"You've been rate limited. Try again in {error.retry_after:.1f} seconds.")
-
-        if interaction.response.is_done():
-            await interaction.followup.send(embed=embed, ephemeral=True)
-        else:
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-        return
-
-    # Handle generic error messages
-    else:
-        embed = Embeds.error(f"An unexpected error occurred.")
-
-        if interaction.response.is_done():
-            await interaction.followup.send(embed=embed, ephemeral=True)
-        else:
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-        return
-
-@client.event
-async def on_ready():
-    Logger.info(f"Connected to Discord as {client.user.name}#{client.user.discriminator}")
 
 if __name__ == "__main__":
     client.run(os.environ.get(config["auth"]["discord_token"]), log_handler=None)
