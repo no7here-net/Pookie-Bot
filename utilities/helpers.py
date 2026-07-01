3ad82571b449005470a02fdc076d540a43272064
no7here <102915075+no7here@users.noreply.github.com>
2026-07-01 10:29:51 +0100

    Remove RCON commands from generic helper - Removed RCON commands from generic helper file as it has been moved to a dedicated minecraft helper file

--- utilities/helpers.py
+++ utilities/helpers.py
@@ -11,8 +11,6 @@ import asyncio
 import json
 import os
 
-from mcrcon import MCRcon
-
 def load_config():
     with open("config.json", "r") as f:
         return json.load(f)
@@ -65,34 +63,6 @@ async def check_host() -> dict:
     # Create KV dictionary of hostnames and results
     return dict(zip(hostnames, results))
 
-# Check Minecraft host(s)
-async def check_rcon() -> dict:
-    # Fetch Minecraft server list from config
-    mc_servers = config.get("minecraft", {}).get("servers", {})
-
-    # If there are no servers, return nothing
-    if not mc_servers:
-        return {}
-
-    # Create dictionary for status to be added to
-    rcon_tasks = []
-
-    # Iterates through all servers in config
-    for name, info in mc.servers.items():
-        server_names.append(name)
-
-        # Fetch password from environment variable
-        password = os.environ.get(details.get("rcon_password"))
-
-        # Queue a background task using internal rcon function
-        rcon_tasks.append(asyncio.to_thread(_sync_check_rcon, info.get("address"), info.get("rcon_port"), password))
-
-    # Run RCON connections simultaneously
-    return = await asyncio.gather(*rcon_tasks)
-
-    # Return a KV dictionary
-    return dict(zip(server_names, results))
-
 # ==================
 # PERMISSION CHECKER
 # ==================
@@ -139,13 +109,5 @@ async def _ping_host(host: str) -> bool:
         )
         await process.wait()
         return process.returncode == 0
-    except Exception:
-        return False
-
-# Internal RCON helper function
-def _sync_check_rcon(host: str, port: int, password: str) -> bool:
-    try:
-        with MCRcon(host, password, port=port) as mcr:
-            return True
     except Exception:
         return False
