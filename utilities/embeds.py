5a6431948f1b7a681666eb48def37cd55ea4aa56
no7here <102915075+no7here@users.noreply.github.com>
2026-07-04 08:12:16 +0100

    Fix spelling mistake + Fixed get_color function being spelt wrong (yes America, you are wrong. And that's a fact.)

--- utilities/embeds.py
+++ utilities/embeds.py
@@ -8,16 +8,16 @@ from utilities.helpers import get_emoji, get_colour
 class Embeds:
     @staticmethod
     def success(description: str) -> discord.Embed:
-        return discord.Embed(description=f"{get_emoji("success")} {description}", color=get_color("success"))
+        return discord.Embed(description=f"{get_emoji("success")} {description}", color=get_colour("success"))
 
     @staticmethod
     def warning(description: str) -> discord.Embed:
-        return discord.Embed(description=f"{get_emoji("warning")} {description}", color=get_color("warning"))
+        return discord.Embed(description=f"{get_emoji("warning")} {description}", color=get_colour("warning"))
 
     @staticmethod
     def error(description: str) -> discord.Embed:
-        return discord.Embed(description=f"{get_emoji("error")} {description}", color=get_color("error"))
+        return discord.Embed(description=f"{get_emoji("error")} {description}", color=get_colour("error"))
 
     @staticmethod
     def info(description: str) -> discord.Embed:
-        return discord.Embed(description=f"{get_emoji("info")} {description}", color=get_color("info"))
+        return discord.Embed(description=f"{get_emoji("info")} {description}", color=get_colour("info"))
