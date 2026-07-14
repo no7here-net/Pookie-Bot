4364eaa7f343c749930362c81c75b0c066a1fbc0
no7here <102915075+no7here@users.noreply.github.com>
2026-07-14 21:18:59 +0100

    More bug fixes & error handling... + Add discord, regex and urllib libraries to import + Corrected database as db + Added fetch_username() to import + Added task as an attribute to check_rcon() + Corrected unsafe config value pulling + Corrected description describing a list as a dictionary + Added safety catches to check_rcon() to prevent mismatched items in the list and to protect against missing password, address or rcon_port values + Added logger to check_rcon() + Added client and user_id to whitelist_logic() + Added username variable via fetch_username() function + Added regex security check to Minecraft username to prevent invalid characters / RCON injection + Changed uuid variable to mc_uuid to be clearer + Corrected _fetch_mc_uuid() error check to catch both "failed" & "unknown" + Added logger warning to error check + Placed database execution into try block + Major formatting and error corrections, there's far too many for me to list here and figure out again (I'm really tired) + Renamed avatar variable to mc_avatar to be clearer + Added info logger event to acknowledge successful whitelist + Updated attribute in _fetch_mc_uuid() from name to mc_username to be clearer + Fixed error in session.head to session.get so it can actually read the response body + Moved session.get to an f-string for better consistency, and added a timeout to prevent infinite hanging + Changed data.get() fallback to unknown, so the sentence is more logical + Added _fetch_mc_username() function which does the same thing as _fetch_mc_uuid() but... with... usernames... + Added mc_username attribute to _fetch_mc_avatar(), and corrected uuid to mc_uuid + Added timeout to prevent infinite hanging + Add task attribute to _send_velocity_command() with default false value + Pass task value to logger classes + Added logging event for missing RCON password + Correct _send_rcon() function name to _sync_send_rcon() for better consistency + Added task attribute to _sync_send_rcon() with default false value + Moved error message catch to log file through new logger error catcher in warning + Added command, host and port information to log so it's much easier to diagnose if an issue occurs + Added task attribute to _sync_check_rcon() with default false value + Added host and port to warning log, and added exception catch like _sync_send_rcon() - Removed setting password to a variable for better security - Removed successful logging event for better consistency - Removed dead continue from Exception catch - Removed dead as mcr from with MCRcon call

--- utilities/minecraft.py
+++ utilities/minecraft.py
@@ -1,38 +1,64 @@
 # Minecraft functions
 # Handles RCON check, whitelist, blacklist (when done) and RCON commands
 
+import discord
 import asyncio
 import aiohttp
 import os
+import re
 
+from urllib.parse import urlparse
 from mcrcon import MCRcon
+
+import utilities.database as db
+
 from utilities.output import Logger
-from utilities.helpers import config
+from utilities.helpers import config, fetch_username
 
 # Check Minecraft host(s)
-async def check_rcon() -> dict:
+async def check_rcon(task: bool = False) -> dict:
     # Fetch Minecraft server list from config
-    mc_servers = config.get("minecraft", {}).get("servers", {})
+    mc_servers = (config.get("minecraft") or {}).get("servers") or {}
 
     # If there are no servers, return nothing
     if not mc_servers:
         return {}
 
-    # Create dictionary for server names
+    # Create list of server names
     server_names = []
 
-    # Create dictionary for status to be added to
+    # Create list of tasks
     rcon_tasks = []
 
     # Iterates through all servers in config
     for name, info in mc_servers.items():
+        # Fetch non-sensitive key name
+        env_key = info.get("rcon_password")
+
+        # If the config is missing the key, or the environment is missing the password
+        if not env_key or not os.environ.get(env_key):
+            Logger.warning(f"Failed to fetch RCON password for \"{name}\" from config.json / environment variables.", task=task)
+            continue
+
+        # Check config data is not missing
+        missing_keys = []
+
+        if not info.get("address"):
+            missing_keys.append("address")
+
+        if not info.get("rcon_port"):
+            missing_keys.append("rcon_port")
+
+        if missing_keys:
+            # Joins the list with " and ", so it handles 1 or 2 items perfectly
+            Logger.warning(f"Failed to fetch \"{"\" and \"".join(missing_keys)}\" for \"{name}\" from config.json.", task=task)
+            continue
+
+        # Append server name if checks pass
         server_names.append(name)
 
-        # Fetch password from environment variable
-        password = os.environ.get(info.get("rcon_password"))
-
         # Queue a background task using internal rcon function
-        rcon_tasks.append(asyncio.to_thread(_sync_check_rcon, info.get("address"), info.get("rcon_port"), password))
+        rcon_tasks.append(asyncio.to_thread(_sync_check_rcon, info.get("address"), info.get("rcon_port"), os.environ.get(env_key), task=task))
 
     # Run RCON connections simultaneously
     results = await asyncio.gather(*rcon_tasks)
@@ -41,72 +67,139 @@ async def check_rcon() -> dict:
     return dict(zip(server_names, results))
 
 # Fetches and links Minecraft & Discord account together
-async def whitelist_logic(discord_id: str, mc_name: str) -> dict:
+async def whitelist_logic(client: discord.Client, user_id: int, mc_username: str) -> dict:
+    # Fetch Discord username from ID provided in attributes. Safe to do here as its value is guarded by the functions that trigger this one.
+    username = await fetch_username(client, user_id)
+
+    if not re.match(r"^[a-zA-Z0-9_]{2,16}$", mc_username):
+        Logger.warning(f"\"{username}\" (ID: {user_id}) was blocked from linking the Minecraft account \"{mc_username}\" due to invalid characters in their Minecraft username.")
+
+        return {
+            "success": False,
+            "error": "Invalid Minecraft username. Usernames can only contain letters, numbers, and underscores."
+        }
+
     # Fetch unique (and unchangeable) ID for the MC account
-    uuid = await _fetch_mc_uuid(mc_name)
+    mc_uuid = await _fetch_mc_uuid(mc_username)
 
     # If it fails to fetch the UUID, error out
-    if uuid == "failed":
+    if mc_uuid in ("failed", "unknown"):
+        Logger.warning(f"\"{username}\" (ID: {user_id}) was blocked from linking the Minecraft account \"{mc_username}\" as their UUID could not be found.")
+
         return {
             "success": False,
             "error": "Failed to find Minecraft account."
         }
 
     # Database security checks
-    async with pool.acquire() as conn:
-        async with conn.cursor() as cur:
-            # Has user already linked an account
-            await cur.execute("SELECT MC_uuid FROM mc_accounts WHERE discord_id = %s", (discord_id,))
+    try:
+        async with db.conn_pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                # Is the Minecraft account banned
+                await cur.execute("SELECT reason FROM mc_bans WHERE mc_uuid = %s", (mc_uuid,))
 
-            if await cur.fetchone():
-                Logger.warning(f"Discord account already linked to a Minecraft account")
-                return {
-                    "success": False,
-                    "error": "Your Discord account is already linked to a Minecraft account."
-                }
+                mc_ban_reason = await cur.fetchone()
 
-            # Is this account matched with someone else
-            await cur.execute("SELECT discord_id FROM mc_accounts WHERE mc_uuid = %s", (uuid,))
+                if mc_ban_reason:
+                    Logger.warning(f"\"{username}\" (ID: {user_id}) was blocked from linking the Minecraft account \"{mc_username}\" (UUID: {mc_uuid}) as it is banned.")
 
-            if await cur.fetchone():
-                Logger.warning(f"Minecraft account already linked to another Discord account.")
-                return {
-                    "success": False,
-                    "error": "This Minecraft account is already linked to another Discord account."
-                }
+                    return {
+                        "success": False,
+                        "error": "This Minecraft account is banned."
+                    }
 
-            # Is the Minecraft account banned
-            await cur.execute("SELECT reason FROM mc_bans WHERE mc_uuid = %s", (uuid,))
+                # Is the Discord account linked to a banned Minecraft account
+                await cur.execute("SELECT reason FROM mc_bans WHERE user_id = %s", (user_id,))
 
-            ban = await cur.fetchone()
+                discord_ban_reason = await cur.fetchone()
 
-            if ban:
-                Logger.warning(f"This Minecraft account is banned.")
-                return {
-                    "success": False,
-                    "error": "This Minecraft account is banned."
-                }
+                if discord_ban_reason:
+                    Logger.warning(f"\"{username}\" (ID: {user_id}) was blocked from linking the Minecraft account \"{mc_username}\" (UUID: {mc_uuid}) as they are banned from linking accounts.")
 
-            # If checks pass
-            response = await _send_velocity_command(f"whitelist add {mc_name}")
+                    return {
+                        "success": False,
+                        "error": "This Discord account is linked to a banned Minecraft account."
+                    }
 
-            if "ERROR" in response:
-                Logger.error(f"Failed to execute whitelist command. Check log.", str(response.strip("ERROR: ")))
-                return {
-                    "success": False,
-                    "error": f"Failed to execute whitelist command."
-                }
+                # Has user already linked an account
+                await cur.execute("SELECT mc_uuid FROM mc_accounts WHERE user_id = %s", (user_id,))
 
-            await cur.execute("INSERT INTO mc_accounts (discord_id, mc_uuid) VALUES (%s, %s)", (discord_id, uuid,))
+                result = await cur.fetchone()
+
+                if result:
+                    # Fetch linked account
+                    linked_mc_uuid = result[0]
+                    linked_mc_username = await _fetch_mc_username(linked_mc_uuid)
+
+                    if linked_mc_username in ("failed", "unknown"):
+                        return {
+                            "success": False,
+                            "error": "Failed to reach Mojang's API for account information."
+                        }
+
+                    Logger.warning(f"\"{username}\" (ID: {user_id}) is already linked to the Minecraft account \"{linked_mc_username}\" (UUID: {linked_mc_uuid}).")
+
+                    return {
+                        "success": False,
+                        "error": "Your Discord account is already linked to a Minecraft account."
+                    }
+
+                # Is this account matched with someone else
+                await cur.execute("SELECT user_id FROM mc_accounts WHERE mc_uuid = %s", (mc_uuid,))
+
+                result = await cur.fetchone()
+
+                if result:
+                    # Fetch account details of the Discord account the Minecraft account is already linked to
+                    linked_user_id = result[0]
+                    linked_username = await fetch_username(client, linked_user_id)
+
+                    Logger.warning(f"\"{username}\" (ID: {user_id}) was blocked from linking the Minecraft account \"{mc_username}\" (UUID: {mc_uuid}) as it is already connected to another account \"{linked_username}\" (ID: {linked_user_id}).")
+
+                    return {
+                        "success": False,
+                        "error": "This Minecraft account is already linked to another Discord account."
+                    }
+    except Exception:
+        Logger.warning(f"\"{username}\" (ID: {user_id}) was blocked from linking the Minecraft account \"{mc_username}\" due to an exception when accessing the database.")
+
+        return {
+            "success": False,
+            "error": "Failed to connect to database to perform checks."
+        }
+
+    # If checks pass
+    response = await _send_velocity_command(f"whitelist add {mc_username}")
+
+    Logger.info(f"\"{username}\" (ID: {user_id}) attempted to add the Minecraft account \"{mc_username}\" to the whitelist. Pushing response to log file.", response)
+
+    # Check response
+    if "Added " not in response:
+        Logger.warning(f"\"{username}\" (ID: {user_id}) failed to execute whitelist command for the Minecraft account \"{mc_username}\" (UUID: {mc_uuid}).")
+
+        return {
+            "success": False,
+            "error": "Failed to execute whitelist command on the server."
+        }
+
+    # Update database & handle any possible issues
+    try:
+        async with db.conn_pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute("INSERT INTO mc_accounts (user_id, mc_uuid) VALUES (%s, %s)", (user_id, mc_uuid,))
+    except Exception:
+        Logger.warning(f"\"{username}\" (ID: {user_id}) did pass whitelist logic and has been whitelisted, but an error occurred when updating the database with the Minecraft account \"{mc_username}\" (UUID: {mc_uuid}).")
 
     # Fetch MC avatar from skin
-    avatar = await _fetch_mc_avatar(uuid)
+    mc_avatar = await _fetch_mc_avatar(mc_username, mc_uuid)
+
+    Logger.info(f"\"{username}\" (ID: {user_id}) successfully whitelisted \"{mc_username}\" (UUID: {mc_uuid}).")
 
     # Return info
     return {
         "success": True,
-        "uuid": uuid,
-        "avatar": avatar,
+        "uuid": mc_uuid,
+        "avatar": mc_avatar,
     }
 
 # ================
@@ -114,71 +207,88 @@ async def whitelist_logic(discord_id: str, mc_name: str) -> dict:
 # ================
 
 # Fetches UUID from username using official API
-async def _fetch_mc_uuid(name: str) -> str:
+async def _fetch_mc_uuid(mc_username: str) -> str:
     async with aiohttp.ClientSession() as session:
         try:
-            async with session.head("https://api.mojang.com/users/profiles/minecraft/" + name) as response:
+            async with session.get(f"https://api.mojang.com/users/profiles/minecraft/{mc_username}", timeout=aiohttp.ClientTimeout(total=2)) as response:
                 if response.status == 200:
                     data = await response.json()
-
-                    Logger.success(f"Found UUID \"{data.get("id", "failed")}\" for \"{name}\".")
-
-                    return data.get("id", "failed")
+                    return data.get("id", "unknown")
+                Logger.warning(f"Failed to fetch UUID for Minecraft username \"{mc_username}\" as response was not \"OK\" (Status: {response.status}).")
+                return "failed"
         except Exception:
-            Logger.warning(f"Failed to fetch UUID for Minecraft username \"{name}\".")
+            Logger.warning(f"Failed to fetch UUID for Minecraft username \"{mc_username}\".")
+            return "failed"
+
+# Fetches username from UUID using official API
+async def _fetch_mc_username(mc_uuid: str) -> str:
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(f"https://sessionserver.mojang.com/session/minecraft/profile/{mc_uuid}", timeout=aiohttp.ClientTimeout(total=2)) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    return data.get("name", "unknown")
+                Logger.warning(f"Failed to fetch username for Minecraft UUID \"{mc_uuid}\" as response was not \"OK\" (Status: {response.status}).")
+                return "failed"
+        except Exception:
+            Logger.warning(f"Failed to fetch username for Minecraft UUID \"{mc_uuid}\".")
             return "failed"
 
 # Fetches avatar by trying multiple 3rd party services
-async def _fetch_mc_avatar(uuid: str) -> str:
+async def _fetch_mc_avatar(mc_username: str, mc_uuid: str) -> str:
     # APIs in order of preference
     apis = [
-        f"https://minotar.net/avatar/{uuid}",
-        f"https://mc-heads.net/avatar/{uuid}"
+        f"https://minotar.net/avatar/{mc_uuid}",
+        f"https://mc-heads.net/avatar/{mc_uuid}"
     ]
 
-    # Try all domains with a timeout of 2
     async with aiohttp.ClientSession() as session:
         for url in apis:
             try:
-                async with session.get(url, timeout=2) as response:
+                async with session.get(url, timeout=aiohttp.ClientTimeout(total=2)) as response:
                     if response.status == 200:
-                        Logger.success(f"Fetched avatar for MC UUID \"{uuid}\".")
                         return url
             except Exception:
-                Logger.warning(f"Failed to fetch avatar for MC UUID \"{uuid}\".")
-                continue
+                Logger.warning(f"Failed to fetch avatar using API \"{urlparse(url).netloc}\" for \"{mc_username}\" (UUID: {mc_uuid}).")
+    Logger.warning(f"Failed to fetch avatar via any API for \"{mc_username}\" (UUID: {mc_uuid}).")
+    return ""
 
 # Send a command to Velocity
-async def _send_velocity_command(command: str) -> str:
+async def _send_velocity_command(command: str, task: bool = False) -> str:
     # Find velocity info from config
-    velocity = config.get("minecraft", {}).get("servers", {}).get("velocity")
+    velocity = ((config.get("minecraft") or {}).get("servers") or {}).get("velocity")
 
     # If there isn't a velocity entry in the config, return nothing
     if not velocity:
-        Logger.warning(f"Velocity proxy not found in config.json")
-        return "ERROR: Velocity server not found in config."
+        Logger.warning("Velocity proxy not found in config.json.", task=task)
+        return "ERROR"
 
-    # Fetch password from environment variables
-    password = os.environ.get(velocity.get("rcon_password"), "")
+    # Fetch non-sensitive key name
+    env_key = velocity.get("rcon_password")
+
+    # If the config is missing the key, or the environment is missing the password
+    if not env_key or not os.environ.get(env_key):
+        Logger.warning("Failed to fetch RCON password for \"velocity\" from config.json / environment variables.", task=task)
+        return "ERROR"
 
     # Return direct result from _rcon_send function
-    return await asyncio.to_thread(_send_rcon, velocity.get("address"), velocity.get("rcon_port"), password, command)
+    return await asyncio.to_thread(_sync_send_rcon, velocity.get("address"), velocity.get("rcon_port"), os.environ.get(env_key), command, task=task)
 
 # Internal RCON broadcast helpers
-def _send_rcon(host: str, port: int, password: str, command: str) -> str:
+def _sync_send_rcon(host: str, port: int, password: str, command: str, task: bool = False) -> str:
     try:
         with MCRcon(host, password, port=port) as mcr:
             response = mcr.command(command)
             return response
     except Exception as e:
-        Logger.error(f"Failed to broadcast. Check log.", str(e))
-        return "ERROR: " + str(e)
+        Logger.warning(f"Failed to broadcast command \"{command}\" to \"{host}:{port}\".", str(e), task=task)
+        return "ERROR"
 
 # Internal RCON helper function
-def _sync_check_rcon(host: str, port: int, password: str) -> bool:
+def _sync_check_rcon(host: str, port: int, password: str, task: bool = False) -> bool:
     try:
-        with MCRcon(host, password, port=port) as mcr:
+        with MCRcon(host, password, port=port):
             return True
-    except Exception:
-        Logger.warning(f"RCON check failed.")
+    except Exception as e:
+        Logger.warning(f"RCON check failed for \"{host}:{port}\".", str(e), task=task)
         return False
