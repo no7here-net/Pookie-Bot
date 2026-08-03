1fb17d0221f41cf0c21cd8630b1046102f8bf5b0
no7here <102915075+no7here@users.noreply.github.com>
2026-08-03 16:31:04 +0100

    Remove error logging from RCON check
    - Removed error message logging from RCON check when an error occurred - turns out it's annoying and doesn't provide any useful info anyway

--- utilities/minecraft.py
+++ utilities/minecraft.py
@@ -640,5 +640,5 @@ def _sync_check_rcon(host: str, port: int, password: str, task: bool = False) ->
         with MCRcon(host, password, port=port):
             return True
     except Exception as e:
-        Logger.warning(f"RCON check failed for \"{host}:{port}\".", str(e), task=task)
+        Logger.warning(f"RCON check failed for \"{host}:{port}\".", task=task)
         return False
